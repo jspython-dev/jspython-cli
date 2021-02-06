554d81145892631d9416bbf7172c324ca62a538f
Pavlo Paska <ppaska@gmail.com>
2021-02-06 20:17:49 +0000

    added src root changes

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 We provide a command line interface to run [JSPython](https://github.com/jspython-dev/jspython) scripts
 
-### Install from NPM
+## Install from NPM
 
 ```
   npm install -g jspython-cli
 ```
 
-### Run in terminal
+## Run in terminal
 ```
   jspython path/to/jspython/file
   jspython --file path/to/jspython/file
@@ -16,7 +16,7 @@ We provide a command line interface to run [JSPython](https://github.com/jspytho
 
 ```
 
-### Pass parameters to script
+## Pass parameters to script
 In CLI
 ```
 jspython --file=path/to/jspython/file --param1=value --param
@@ -28,15 +28,67 @@ params("param1") == "value" # true
 params("param") == false # true
 ```
 
-### Save evaluate log into file
+## Run file
+```
+jspython --file=path/to/jspython/file.jspy
+```
+
+## Run file and log into file
 ```
 jspython --file=path/to/jspython/file.jspy --output=path/to/log.txt
 ```
 
-### Development
+## Run specific function from the file
+```
+jspython --file=path/to/jspython/file.jspy --entryFunction=myFunc1
+```
+or
+```
+jspython -f=path/to/jspython/file.jspy -e=myFunc1
+```
+
+
+
+## Run file when you have your source code in a nested folder
+```
+jspython --file=path/to/jspython/file.jspy --srcRoot=src
+```
+In this case it expects package.json and node_modules on the root level
+
+```
+-|- .git
+-|- .vscode
+-|- .gitignore
+-|- .ws
+-|- node_modules
+-|- src
+     -|-my_code.jspy
+-|- package.json
+```
+
+Then, from a root folder you can:
+> jspython --file=my_code.jspy --srcRoot=src --param1=some_Value
+
+or
+
+> jspython -f=my_code.jspy -s=src --param1=some_Value
+
+
+## Show version
+
+> jspython -v
+
+or
+
+> jspython --version
+
+## Development
 Run example using node. (Works only if you have build project `npm run build`)
 ```
 node ./bin/jspython --file=../jspython-examples/axios-test.jspy
 node ./bin/jspython --file ../jspython-examples/parse.jspy
 ```
+
+# License
+A permissive [BSD 3-Clause License](https://github.com/jspython-dev/jspython-cli/blob/master/LICENSE) (c) FalconSoft Ltd.
 
--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,34 @@ const context: any = {
   params: {}
 }
 
-const appFolder = process.cwd().split('\\').join('/');
 
 const initialScope: Record<string, any> = {
   app: {}
 };
 
+const rootFolder = process.cwd().split('\\').join('/');
 const interpreter: Interpreter = jsPython() as Interpreter;
+const options = getOptionsFromArguments(process.argv);
 
 run();
 
-async function initialize() {
+function trimChar(text: string, charToRemove: string): string {
+  while (text.charAt(0) == charToRemove) {
+    text = text.substring(1);
+  }
+
+  while (text.charAt(text.length - 1) == charToRemove) {
+    text = text.substring(0, text.length - 1);
+  }
+
+  return text;
+}
+
+async function initialize(baseSource: string) {
   // process app.json (if exists)
   // add configuration to the 'app'
-  if (fs.existsSync(`${appFolder}/app.json`)) {
-    const app = require(`${appFolder}/app.json`);
+  if (fs.existsSync(`${rootFolder}/${baseSource}app.json`)) {
+    const app = require(`${rootFolder}/${baseSource}app.json`);
     initialScope.app = Object.assign(initialScope.app || {}, app);
   }
 
@@ -33,8 +46,8 @@ async function initialize() {
   //  - run _initAsync
   //  - delete _initAsync
   //  - load content into 'app'
-  if (fs.existsSync(`${appFolder}/app.js`)) {
-    const app = require(`${appFolder}/app.js`);
+  if (fs.existsSync(`${rootFolder}/${baseSource}app.js`)) {
+    const app = require(`${rootFolder}/${baseSource}app.js`);
 
     if (typeof app._init == 'function') {
       app._init();
@@ -58,9 +71,6 @@ async function initialize() {
 }
 
 async function run() {
-  await initialize();
-
-  const options = getOptionsFromArguments(process.argv);
   if (options.version) {
     console.log(interpreter.jsPythonInfo());
     console.log(`JSPython cli v${(pkg || {}).version}\n`);
@@ -79,15 +89,26 @@ async function run() {
     console.error = console.log;
   }
 
+  await initialize(options.srcRoot);
+
   if (options.file) {
+    interpreter.registerFileLoader((jspyPath: any) => {
+      console.log(' * file loader * ', rootFolder, jspyPath);
+      return Promise.resolve({ sum: (x: number, y: number) => x + y });
+    })
     interpreter.registerPackagesLoader(packageLoader as PackageLoader);
-    const scripts = fs.readFileSync(options.file, 'utf8');
+    const scripts = fs.readFileSync(`${options.srcRoot}${options.file}`, 'utf8');
     context.asserts.length = 0;
     console.log(interpreter.jsPythonInfo())
     console.log(`> ${options.file}`)
-    const res = await interpreter.evaluate(scripts, initialScope, options.entryFunction || undefined, options.file);
-    if (res !== null) {
-      console.log(res);
+    try {
+      const res = await interpreter.evaluate(scripts, initialScope, options.entryFunction || undefined, options.file);
+      if (res !== null) {
+        console.log(res);
+      }
+    } catch (err) {
+      console.log('JSPython execution failed: ', err?.message || err, err);
+      throw err;
     }
   }
 }
@@ -95,11 +116,13 @@ async function run() {
 function getOptionsFromArguments(rawArgs: string[]) {
   const args = arg({
     '--file': String,
+    '--srcRoot': String,
     '--entryFunction': String,
     '--version': Boolean,
     '--output': String,
     '-f': '--file',
-    '-ef': '--entryFunction',
+    '-s': '--srcRoot',
+    '-e': '--entryFunction',
     '-v': '--version',
     '-o': '--output'
   }, {
@@ -121,8 +144,14 @@ function getOptionsFromArguments(rawArgs: string[]) {
     file: args['--file'] || (rawArgs.length === 3 && !rawArgs[2].startsWith('-') ? rawArgs[2] : ''),
     version: args['--version'],
     output: args['--output'],
-    entryFunction: args['--entryFunction']
+    entryFunction: args['--entryFunction'],
+    srcRoot: args['--srcRoot'] || ''
   };
+
+  res.srcRoot = trimChar(res.srcRoot || '', '/');
+  if (res.srcRoot.length) {
+    res.srcRoot = res.srcRoot + '/';
+  }
 
   context.params = { ...res, ...params };
 
@@ -138,13 +167,13 @@ function packageLoader(packageName: string): any {
     }
 
     if (packageName.toLowerCase().endsWith('.js') || packageName.toLowerCase().endsWith('.json')) {
-      return require(`${process.cwd().split('\\').join('/')}/${packageName}`)
+      return require(`${rootFolder}/${options.srcRoot}${packageName}`)
     }
 
-    return require(`${process.cwd().split('\\').join('/')}/node_modules/${packageName}`);
+    return require(`${rootFolder}/node_modules/${packageName}`);
   }
   catch (err) {
-    console.log('Import Error: ', err);
+    console.log('Import Error: ', err?.message || err);
     throw err;
   }
 }
